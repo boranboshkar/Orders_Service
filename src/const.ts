2894f714239d6ec08910b4a6552bf2360d385a4a
boranboshkar <boran.b@campus.technion.ac.il>
2024-11-27 00:57:06 +0200

    updated the DB

--- src/const.ts
+++ src/const.ts
@@ -1,5 +1,5 @@
-export const CREATE_ORDER_PATH = '/orders';
-export const GET_ORDER_DETAILS_PATH = '/orders';
-export const GET_USER_ORDERS_PATH = '/orders/user';
-export const PROCESS_PAYMENT_PATH = '/orders/payment';
-export const REQUEST_REFUND_PATH = '/orders/refund';
+export const CREATE_ORDER_PATH = '/Orders';
+export const GET_ORDER_DETAILS_PATH = '/Orders';
+export const GET_USER_ORDERS_PATH = '/Orders/user';
+export const PROCESS_PAYMENT_PATH = '/Orders/payment';
+export const REQUEST_REFUND_PATH = '/Orders/refund';
